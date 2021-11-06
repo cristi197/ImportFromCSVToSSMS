0ee36cbccfdbb10179e2b10c70393863dd336921
Cristi Teodorescu <cristian.teodorescu97@yahoo.ro>
2021-11-06 19:51:23 +0200

    Create app for import csv to ssms

---
+++ ExportFromCSVToSSMS/ExportFromCSVToSSMS/ConnectToDatabase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ExportFromCSVToSSMS
+{
+    class ConnectToDatabase
+    {
+
+        static public void InsertIntoDatabase(string UserName, string FirstName, string LastName)
+        {
+
+            try
+            {
+                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultDatabase"].ToString());
+                SqlCommand cmd = new SqlCommand("InsertIntoAccounts", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@UserName", UserName);
+                cmd.Parameters.AddWithValue("@FirstName", FirstName);
+                cmd.Parameters.AddWithValue("@LastName", LastName);
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Something was wrong! \nMessage:{0}",ex.Message);
+            }
+        }
+
+    }
+}
---
+++ ExportFromCSVToSSMS/ExportFromCSVToSSMS/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace ExportFromCSVToSSMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var dt = DataTable.New.ReadLazy(@"..\..\username.csv");
+            List<Account> accounts = new List<Account>();
+   
+
+            for (int e = 0; e < dt.Rows.Count();e++)
+            {
+                Account account = new Account();
+
+                account.UserName = dt.Rows.ElementAt(e).Values[0];
+                account.FirstName = dt.Rows.ElementAt(e).Values[2];
+                account.LastName = dt.Rows.ElementAt(e).Values[3];
+
+                accounts.Add(account);
+
+            }
+
+            foreach(var acc in accounts)
+            {
+                ConnectToDatabase.InsertIntoDatabase(acc.UserName, acc.FirstName, acc.LastName);
+            }
+
+            Console.WriteLine("Totul s-a important cu succes in baza de date!");
+        }
+    }
+}
