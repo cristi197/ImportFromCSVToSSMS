95c9f14e1129810e477414980e380de1b280f4d7
Cristi Teodorescu <cristian.teodorescu97@yahoo.ro>
2021-12-01 12:42:41 +0200

    Add filter to Movies

--- AngularMovie/angular-movies/src/app/movies/movie-filter/movie-filter.component.ts
+++ AngularMovie/angular-movies/src/app/movies/movie-filter/movie-filter.component.ts
@@ -14,6 +14,15 @@ export class MovieFilterComponent implements OnInit {
 
   genres = [{id:1, name: 'Drama'},{id:2, name:'Action'}];
 
+  //take a movies array to test our filter
+  movies = [
+    {title:'Spider-Man', poster: 'https://m.media-amazon.com/images/M/MV5BZDEyN2NhMjgtMjdhNi00MmNlLWE5YTgtZGE4MzNjMTRlMGEwXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_FMjpg_UX1000_.jpg'} ,
+    {title:'Moana', poster: 'https://i.pinimg.com/originals/0b/aa/31/0baa317b04030e357d1bd687a52cfce9.jpg'} ,
+    {title:'007', poster: 'https://m.media-amazon.com/images/M/MV5BMWZiNjE2OWItMTkwNy00ZWQzLWI0NTgtMWE0NjNiYTljN2Q1XkEyXkFqcGdeQXVyNzAwMjYxMzA@._V1_FMjpg_UX1000_.jpg'} 
+];
+
+  originalMovies = this.movies;
+
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       title:'',
@@ -21,10 +30,23 @@ export class MovieFilterComponent implements OnInit {
       upcomingReleases: false,
       inTheaters: false 
     });
+
+
+    this.form.valueChanges //values of the form from above
+      .subscribe(values =>{
+        this.movies = this.originalMovies;
+        this.filterMovies(values);
+      });
+  }
+
+  filterMovies(values: any){ 
+    if(values.title){ //sort by title
+      this.movies = this.movies.filter(movie => movie.title.indexOf(values.title) !== -1)
+    }
   }
 
   clearForm(){
-    
+    this.form.reset();
   }
 
 }
